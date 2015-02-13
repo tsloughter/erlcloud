e68643ec90cb19557c3cc99d98888b4a2bd77df0
Tristan Sloughter <t@crashfast.com>
2015-02-13 12:26:12 -0600

    remove old form of crypto functions

--- src/erlcloud_util.erl
+++ src/erlcloud_util.erl
@@ -3,38 +3,13 @@
          md5/1, sha256/1]).
 
 sha_mac(K, S) ->
-    try
-        crypto:hmac(sha, K, S)
-    catch
-        error:undef ->
-            R0 = crypto:hmac_init(sha, K),
-            R1 = crypto:hmac_update(R0, S),
-            crypto:hmac_final(R1)
-    end.
-        
+    crypto:hmac(sha, K, S).
+
 sha256_mac(K, S) ->
-    try
-        crypto:hmac(sha256, K, S)
-    catch
-        error:undef ->
-            R0 = crypto:hmac_init(sha256, K),
-            R1 = crypto:hmac_update(R0, S),
-            crypto:hmac_final(R1)
-    end.
+    crypto:hmac(sha256, K, S).
 
 sha256(V) ->
-    try
-        crypto:hash(sha256, V)
-    catch
-        _:_ ->
-            crypto:sha256(V)
-    end.
+    crypto:hash(sha256, V).
 
 md5(V) ->
-    try
-        crypto:hash(md5, V)
-    catch
-        _:_ ->
-            crypto:md5(V)
-    end.
-     
+    crypto:hash(md5, V).
